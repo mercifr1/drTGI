207fee1f11ab5c4df334b1c61b02a16e1d300f9e
Maia-Iulia <muresan.maia@gmail.com>
2022-05-05 17:15:38 +0200

    Compare simulation on Analytical form with and without SIGMA

--- Maia/Rscripts/SimAnalyticalwithTIME.R
+++ Maia/Rscripts/SimAnalyticalwithTIME.R
@@ -11,31 +11,40 @@ library(brms)
 
 #'Define the structure of the model
 
-code <- "
+#'Without SIGMA
+#NOTE : we can still capture EPS(1), because by default $SIGMA matrix in 0
+
+pred<- "
 $PARAM    TVKG =0.6, TVKS0=0.4, TVGAMA=0.8, TVBASE =70 
 $PARAM@covariates
 DOSE=10
 
 $OMEGA  0.005 0.03 0.003 0.01
 
-$SIGMA 0
 
 $PRED
 double KG = TVKG * exp(ETA(1));
 double KS0 = TVKS0 * exp(ETA(2));
 double GAMA = TVGAMA * exp(ETA(3));
 double BASE=TVBASE * exp(ETA(4));
-capture RESP_0=BASE;
+double RESP_0=BASE;
 
 double KS =  KS0 * log(DOSE) * exp(-GAMA * TIME);
-capture RESP = RESP_0*(exp((KG*TIME)-( KS0 * log(DOSE)/GAMA) + KS/GAMA))+EPS(1);
+double RESP = RESP_0*(exp((KG*TIME)-( KS0 * log(DOSE)/GAMA) + KS/GAMA));
 
 
 
-$CAPTURE    RESP_0 KS KS0 KG GAMA EPS(1)
+$CAPTURE    RESP RESP_0 BASE KS KS0 KG GAMA EPS(1)
 "
+# compile the model
+set.seed(123)
+model_pred<-mcode("PRED",pred)
 
-cod<- "
+
+# With SIGMA
+# NOTE: we add  +EPS(1) to the RESP, compared to the <pred> model
+
+pred_sigma<- "
 $PARAM    TVKG =0.6, TVKS0=0.4, TVGAMA=0.8, TVBASE =70 
 $PARAM@covariates
 DOSE=10
@@ -49,42 +58,26 @@ double KG = TVKG * exp(ETA(1));
 double KS0 = TVKS0 * exp(ETA(2));
 double GAMA = TVGAMA * exp(ETA(3));
 double BASE=TVBASE * exp(ETA(4));
-capture RESP_0=BASE;
+double RESP_0=BASE;
 
 double KS =  KS0 * log(DOSE) * exp(-GAMA * TIME);
-capture RESP = RESP_0*(exp((KG*TIME)-( KS0 * log(DOSE)/GAMA) + KS/GAMA))+EPS(1);
+double RESP = RESP_0*(exp((KG*TIME)-( KS0 * log(DOSE)/GAMA) + KS/GAMA))+EPS(1);
 
 
 
-$CAPTURE    RESP_0 KS KS0 KG GAMA EPS(1)
+$CAPTURE    RESP RESP_0 BASE KS KS0 KG GAMA EPS(1)
 "
 
-
-#' Compile the model
-#' ------------------------------------------------------
+#Compile the model
 set.seed(123)
-mod <- mcode("maia", code)
-mod
-see(mod)
-init(mod)
-param(mod)
-revar(mod)
-omat(mod)
-smat(mod)
+model_pred_sigma<-mcode("PREDSIGMA",pred_sigma)
 
 
-set.seed(123)
-model<-mcode("maia2",cod)
-model
-revar(model)
-smat(model)
-#' Time matrix
+
+#' Construction of Time matrix
 #'----------------------------------------------------
-#'
-#'
 
-#' **Nb of Ind must corresponds to nb of doses in this program**
-nInd<-5
+nInd<-5 #number of individual
 
 #' buid a function to define the time 
 funtime<-function(end,delta){
@@ -94,7 +87,6 @@ funtime<-function(end,delta){
 }
 # funtime(24,0.1)
 
-
 indf<-tibble(ID=1:nInd) %>%
   split(.$ID) %>%
   map_dfr(., ~funtime(1,1/10), .id="ID")
@@ -103,8 +95,7 @@ head(indf,n=15)
 
 #' Design matrix: one level dose for all patients for all time points
 #'----------------------------------------------------
-#'
-#'
+
 
 #' **To each ID corresponds a different dose e.g. 5 ID = 5 doses**
 
@@ -118,107 +109,48 @@ flt<-indf %>%
   
 head(flt,n=15)
 
-#' Design matrix: 2 dose levels at a certain time point for all patients
-#' ---------------------------------------------------
-
-stepup0<-flt%>%mutate(DOSE=ifelse(tnum==5,5,10.00))
-head(stepup0,n=15)
-
-
-#' Design matrix: 5 dose levels at a certain time point for all patients
-#' ---------------------------------------------------
-#' 
-stepup1<-flt%>%mutate(DOSE=ifelse(tnum==2 | tnum==4|tnum==6| tnum==8,5,10))
-head(stepup1,n=15)
-
-
 
 
 
 #' Simulation 
 #' ---------------------------------------------------
 #' 
-#' 
 
-#' **1. For the flat dose with SIGMA=0**
-#' 
-
-
+#' Simulation for pred ( without SIGMA)
 set.seed(123)
-#ssim data set
-snd.flt<-mrgsim_d(mod, as.data.frame(flt), carry.out="KS,DOSE,RESP_0,EPS(1)") %>% 
+snd.flt<-mrgsim_d(model_pred, as.data.frame(flt), carry.out="KS,DOSE,RESP_0,BASE") %>% 
   as.data.frame()
 head(snd.flt,n=20)
 
+# Conclusion: <KS> in varying over <time> ( what we expected)
+#           : <BASE> and <RESP_0> have the same value ( what we expected)
+#           : <EPS(1)> is 0 overall
+#           : <RESP> values are correct when we replace all the parameters inside the formula
+# Ex: for time=0.4: RESP_0*(exp((KG*TIME)-( KS0 * log(DOSE)/GAMA) + KS/GAMA))+EPS(1):
+
+#     77.55688=83.90358*(exp((0.6429033 *0.4)-( 0.4236797  * log(10)/0.7705246) + 0.7168025/0.7705246))+0
+
+# plot
 set.seed(123)
-# plot the data with SIGMA=0
-mrgsim_d(mod, as.data.frame(flt), carry.out="KS,DOSE,RESP_0")%>%plot(RESP~time)
+mrgsim_d(model_pred, as.data.frame(flt), carry.out="KS,DOSE,RESP_0,BASE") %>% plot(RESP~time)
 
 
-set.seed(123)
-#plot the data when force the sigma to be 0 by using the function zero_re()
-mrgsim_d(mod, as.data.frame(flt), carry.out="KS,DOSE,RESP_0",zero_re(sigma))%>%plot(RESP~time)
 
-#' **1. For the flat dose wit SIGMA=0.05**
-#' 
+#'Simulation for pred_sigma ( with SIGMA 0.05)
 set.seed(123)
 
-snd.flt_2<-mrgsim_d(model, as.data.frame(flt), carry.out="KS,DOSE,RESP_0,EPS(1)") %>% 
+snd.flt_sigma<-mrgsim_d(model_pred_sigma, as.data.frame(flt)) %>% 
   as.data.frame()
-head(snd.flt_2,n=20)
+head(snd.flt_sigma,n=20)
+
+
+#Conclusion: KS is the same as in the previous data ( correct)
+#          : BASE and RESP_0 remains the same as before ( correct )
+#          : EPS(1) is changing by individual over <time>
+#          : <RESP> values are correct when we replace the parameters + <EPS(1)> 
+#            inside the formula, and they are different from the <RESP> from the 
+#            simulation without SIGMA
+# Ex: for time=0.4: 
+#     77.45759=83.90358 *(exp((0.6429033 *0.4)-( 0.4236797  * log(10)/0.7705246 ) + 0.7168025/0.7705246 ))+0.222903976
 set.seed(123)
-
-mrgsim_d(model, as.data.frame(flt), carry.out="KS,DOSE,RESP_0")%>%plot(RESP~time)
-
-set.seed(123)
-
-mrgsim_d(model, as.data.frame(flt), carry.out="KS,DOSE,RESP_0",zero_re(sigma))%>%plot(RESP~time)
-set.seed(123)
-
-model%>%zero_re(sigma)%>%data_set(flt)%>%mrgsim()%>%plot(RESP~time)
-set.seed(123)
-
-model%>%%>%data_set(flt)%>%mrgsim()%>%plot(RESP~time)
-
-
-#' **2. For the 2 level dose**
-#' 
-set.seed(123)
-
-snd.stepup0<-mrgsim_d(mod, as.data.frame(stepup0)) %>% 
-  as.data.frame()
-
-head(snd.stepup0,n=10)
-
-set.seed(123)
-mrgsim_d(mod, as.data.frame(stepup0))%>%plot(RESP~time)
-
-# SIGMA=0.05
-set.seed(123)
-
-snd.stepup0_2<-mrgsim_d(model, as.data.frame(stepup0)) %>% 
-  as.data.frame()
-
-head(snd.stepup0_2,n=10)
-
-set.seed(123)
-mrgsim_d(model, as.data.frame(stepup0))%>%plot(RESP~time)
-
-
-
-#' **3. For the 5 dose levels**
-#' 
-#' 
-
-set.seed(123)
-
-snd.stepup1<-mrgsim_d(mod, as.data.frame(stepup1), carry.out="KS,DOSE") %>% 
-  as.data.frame()%>%
-  subset(select=-c(time))
-
-head(snd.stepup1,n=10)
-
-set.seed(123)
-mrgsim_d(mod, as.data.frame(stepup1), carry.out="KS,DOSE")%>%plot(RESP~T)
-
-
+mrgsim_d(model_pred_sigma, as.data.frame(flt)) %>% plot(RESP~time)
