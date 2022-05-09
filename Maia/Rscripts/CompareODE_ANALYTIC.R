7aa5e52596abd9959dbbabac8bc29267b02cbf15
Maia-Iulia <muresan.maia@gmail.com>
2022-05-09 10:48:45 +0200

    Update comparison both simulations

--- Maia/Rscripts/CompareODE_ANALYTIC.R
+++ Maia/Rscripts/CompareODE_ANALYTIC.R
@@ -11,19 +11,21 @@ ODE<-snd.flt_ODE_sigma
 head(ODE,n=20)
 
 ODE<-ODE%>%subset(select=-c(RESP))%>%rename(RESP=DV)
-head(ODE1)
+head(ODE)
 
 ANALYTIC<-snd.flt_sigma
 
 
 
-head(ANALYTIC)
+head(ANALYTIC,n=20)
 head(ODE,n=20)
 
 
 
 set.seed(123)
 
-#par(mfrow=c(2,2))
+par(mfrow=c(2,1))
+set.seed(123)
 mrgsim_d(model_pred_sigma, as.data.frame(flt)) %>% plot(RESP~time)
+set.seed(123)
 mrgsim_d(model_ode_sigma, as.data.frame(flt)) %>% plot(DV~time)
