d4a6b716049c33a364479b7d29dfcc4433b34b8f
Maia-Iulia <muresan.maia@gmail.com>
2022-06-01 11:30:06 +0200

    Update the brms for PRED with the new notations

--- Maia/Rscripts/BRMSAnalytic.R
+++ Maia/Rscripts/BRMSAnalytic.R
@@ -1,4 +1,4 @@
-source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/SimAnalytical.R")
+source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/SimPRED.R")
 
 
 #'Fit with brms 
@@ -12,11 +12,11 @@ source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/SimAnalytical.R")
 #'-----------------------------------------------------------------------------
 #' log-transformation of the parameters
 new_data<-snd.flt%>%
-  mutate(lKG=log(KG),lKS0=log(KS0),lGAMA=log(GAMA),lBAS=log(BASE))#%>%subset(select=-c(KS0,KG,GAMA,BASE))
+  mutate(lKG=log(KG),lKS0=log(KS0),lGAMMA=log(GAMMA),lBAS=log(BASE))%>%subset(select=-c(KS0,KG,GAMMA,BASE))
 names(new_data)
 
 #' Define the model structure and parameters < random effects>
-analytic <- bf(RESP ~ exp(lBAS) *  exp( exp(lKG) * T - ( exp(lKS0) *log(DOSE)/ exp(lGAMA) ) *( 1 - exp(exp(lGAMA)*T)))  ,
+analytic <- bf(RESP ~ exp(lBAS) *  exp( exp(lKG) * TIME - ( exp(lKS0) *log(DOSE)/ exp(lGAMA) ) *( 1 - exp(exp(lGAMA)*TIME)))  ,
                nl = TRUE, 
                lBAS ~ 1+ (1|ID),
                lKG ~ 1 + (1 | ID),
@@ -30,14 +30,14 @@ analytic
 #'
 #'
 # check which  priors
-getpr1<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * T - ( exp(lKS0) *log(DOSE)/ exp(lGAMA) ) *( 1 - exp(exp(lGAMA)*T)))  ,
+getpr1<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * TIME - ( exp(lKS0) *log(DOSE)/ exp(lGAMMA) ) *( 1 - exp(exp(lGAMMA)*TIME)))  ,
                   data = new_data, family = gaussian())
 
 
-getpr2<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * T - ( exp(lKS0) *log(DOSE)/ exp(lGAMA) ) *( 1 - exp(exp(lGAMA)*T)))  ,
+getpr2<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * TIME - ( exp(lKS0) *log(DOSE)/ exp(lGAMMA) ) *( 1 - exp(exp(lGAMMA)*TIME)))  ,
                   data = new_data, family = lognormal())
 
-getpr3<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * T - ( exp(lKS0) *log(DOSE)/ exp(lGAMA) ) *( 1 - exp(exp(lGAMA)*T)))  ,
+getpr3<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * TIME - ( exp(lKS0) *log(DOSE)/ exp(lGAMMA) ) *( 1 - exp(exp(lGAMMA)*TIME)))  ,
                   data = new_data, family = student())
 
 # # first try: it is not converging:max(Rhat) is  > 3
@@ -62,22 +62,22 @@ getpr3<-get_prior(RESP ~ exp(lBAS) *  exp( exp(lKG) * T - ( exp(lKS0) *log(DOSE)
 # second try:It converge: Rhat=1 
 
 #' Define the model structure and parameters < random effects>
-analytic2 <- bf(RESP ~ exp(lBAS) *  exp( (exp(lKG)/100) * T - ( (exp(lKS0)/100) *log(DOSE)/ (exp(lGAMA)/100) ) *( 1 - exp((exp(lGAMA)/100)*T)))  ,
+analytic2 <- bf(RESP ~ exp(lBAS) *  exp( (exp(lKG)/100) * TIME - ( (exp(lKS0)/100) *log(DOSE)/ (exp(lGAMMA)/100) ) *( 1 - exp((exp(lGAMMA)/100)*TIME)))  ,
                 nl = TRUE, 
                 lBAS ~ 1+ (1|ID),
                 lKG ~ 1 + (1 | ID),
                 lKS0 ~ 1 + (1 | ID),
-                lGAMA ~ 1 + (1 | ID))
+                lGAMMA ~ 1 + (1 | ID))
 analytic2
 
 prior2<-set_prior("normal(4,1)",class="b",nlpar="lBAS")+
   set_prior("normal(0,1)",class="b",nlpar="lKG")+
   set_prior("normal(0,1)",class="b",nlpar="lKS0")+
-  set_prior("normal(0,1)",class="b",nlpar="lGAMA")+
+  set_prior("normal(0,1)",class="b",nlpar="lGAMMA")+
   set_prior("student_t(3, 0, 2.5)",class="sd",nlpar="lBAS")+
   set_prior("student_t(3, 0, 2.5)",class="sd",nlpar="lKG")+
   set_prior("student_t(3, 0, 2.5)",class="sd",nlpar="lKS0")+
-  set_prior("student_t(3, 0, 2.5)",class="sd",nlpar="lGAMA")
+  set_prior("student_t(3, 0, 2.5)",class="sd",nlpar="lGAMMA")
 prior2
 
 m02<-brm(analytic2, data=new_data, prior=prior2, family=lognormal(link = "identity"),
@@ -94,8 +94,8 @@ pairs(m02)
 #' 
 
 
-new_data_2<-snd.stepup0%>%
-  mutate(lKG=log(KG),lKS0=log(KS0),lGAMA=log(GAMA),lBAS=log(BASE))#%>%subset(select=-c(KS0,KG,GAMA,BASE))
+new_data_2<-snd.tv%>%
+  mutate(lKG=log(KG),lKS0=log(KS0),lGAMA=log(GAMMA),lBAS=log(BASE))%>%subset(select=-c(KS0,KG,GAMMA,BASE))
 names(new_data_2)
 head(new_data_2)
 
