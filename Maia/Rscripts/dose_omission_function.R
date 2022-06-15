e33fcd3655aa93231ace5f20b67201afe5a96b6f
Maia-Iulia <muresan.maia@gmail.com>
2022-06-15 21:25:55 +0200

    add the dose modification on cohort 5: 2 dosing omission in a row

--- Maia/Rscripts/dose_omission_function.R
+++ Maia/Rscripts/dose_omission_function.R
@@ -1,46 +1,78 @@
 library(tidyr)
 library(extraDistr)
 
+dose_omission <- function(initial_data, cohort, proportion, omission, n_unif_omis, min_unif_omis, max_unif_omis, seed_unif_omis) {
+  set.seed(seed_unif_omis)
 
-dose_omission<-function(initial_data, cohort, proportion, omission){
-  
-  data_1 <- initial_data %>% #' data is from Scenario_2A
-    dplyr::filter(coh == cohort) %>% 
-    dplyr::group_by(ID) %>% 
-    mutate(TIME_unif_omis = round(runif(1, 7 * 12 / 356, 7 * 24 / 356), 3)) %>% 
+  data_1 <- initial_data %>% #' initial_data is from Scenario_1
+    dplyr::filter(coh == cohort) %>%
+    dplyr::group_by(ID) %>%
+    mutate(TIME_unif_omis = round(x = runif(
+      n = n_unif_omis,
+      min = min_unif_omis,
+      max = max_unif_omis
+    ), digits = 3)) %>%
     ungroup()
-  
-  
-  #' sample the proportion of subjects per cohort to receive the dose_omision
-  id_proportion<-proportion * length(unique(data_1$ID))
-  nsel<-ifelse(id_proportion-floor(id_proportion)<0.5,floor(id_proportion),ceiling(id_proportion))
-  
-  mysel_omission <- data_1 %>%
-    dplyr::sample_n(ID, size = nsel, replace = F)
-  
-  data_1 %>%
+
+
+  #' sample the proportion of subjects per cohort to receive the dose_omission
+  id_proportion <- proportion * length(unique(data_1$ID))
+  nsel <- ifelse(id_proportion - floor(id_proportion) < 0.5, floor(id_proportion), ceiling(id_proportion))
+
+  mysel_omission <- sample(data_1$ID, size = nsel, replace = F)
+
+  data_omis <- data_1 %>%
+    group_by(ID) %>%
     mutate(
-      IDflag_omis = ifelse(ID %in% mysel_omission, 1, 0),#flag for the suject in question
-      TIMEflag_omis=ifelse( TIME>=TIME_unif_omis,1,0))%>% # flag for the time condition
-    
-    group_by(ID)%>%#  manipulation by ID group
-    mutate(IDTIMEflag_omis = ifelse(IDflag_omis==1 & TIMEflag_omis==1,1, 0), #flag ID and TIME condition 
-           ONE_REP=as.numeric(!duplicated(IDTIMEflag_omis)), # in case there are multiple conditions, select only the first equality
-           DOSEflag_omis=ifelse(ONE_REP==1 & IDTIMEflag_omis==1,2,0),
-           DOSE_omis=ifelse(DOSEflag_omis==2,omission,DOSE)#' one dose omission for those patients and at the specific time
-    )%>%
+      IDflag_omis = ifelse(ID %in% mysel_omission, 1, 0), # flag for the suject in question
+      TIMEflag_omis = ifelse(IDflag_omis == 1 & TIME >= TIME_unif_omis, 1, 0),
+      DOSEflag_omis = {
+        TIMEflag_omis == 1
+      } %>%
+        {
+          . * !duplicated(.)
+        },
+      DOSE_omis = ifelse(DOSEflag_omis == 1, omission, DOSE)
+    ) %>%
     ungroup()
-  
-  
-  
-  
-  
+
+  for (i in unique(data_omis$ID)) {
+    try1 <- filter(data_omis, ID == i & coh == 5) %>%
+      pull(DOSE_omis)
+    if (any(try1 == 0.001)) {
+      place <- which(try1 == 0.001)
+      if (length(try1) > place) {
+        try1[place + 1] <- 0.001
+      }
+    }
+    data_omis[data_omis$ID == i & data_omis$coh == 5, "DOSE_omis"] <- try1
+  }
+
+  data_omis
 }
 
-data_bind_omis<- rbind(
-  dose_omission(initial_data = full_data, cohort = 3, proportion = 0.10, omission = 0.001),
-  dose_omission(initial_data = full_data, cohort = 4, proportion = 0.25, omission = 0.001),
-  dose_omission(initial_data = full_data, cohort = 5, proportion = 0.50, omission = 0.001)
+
+
+data_bind_omis <- rbind(
+  dose_omission(
+    initial_data = full_data,
+    cohort = 3,
+    proportion = 0.10,
+    omission = 0.001,
+    n_unif_omis = 1, min_unif_omis = 7 * 12 / 356, max_unif_omis = 7 * 24 / 356, seed_unif_omis = 123
+  ),
+  dose_omission(
+    initial_data = full_data,
+    cohort = 4,
+    proportion = 0.25,
+    omission = 0.001,
+    n_unif_omis = 1, min_unif_omis = 7 * 12 / 356, max_unif_omis = 7 * 24 / 356, seed_unif_omis = 124
+  ),
+  dose_omission(
+    initial_data = full_data,
+    cohort = 5,
+    proportion = 0.50,
+    omission = 0.001,
+    n_unif_omis = 1, min_unif_omis = 7 * 12 / 356, max_unif_omis = 7 * 24 / 356, seed_unif_omis = 125
+  )
 )
-
-
--- Maia/Rscripts/dose_reduction_function.R
+++ Maia/Rscripts/dose_reduction_function.R
@@ -1,32 +1,62 @@
 
-dose_reduction <- function(initial_data, cohort, proportion, reduction) {
+
+dose_reduction <- function(initial_data, cohort, proportion, reduction, n_unif_omis, min_unif_omis,max_unif_omis) {
+  
+  
   data_1 <- initial_data %>%
     filter(coh == cohort) %>%
     group_by(ID) %>%
-    mutate(TIME_unif_reduc = round(runif(1, 7 * 12 / 356, 7 * 24 / 356), 3)) %>%
+    mutate(TIME_unif_reduc = round(x=runif(n=n_unif_omis, #generate 1 random uniform number per ID
+                                           min=min_unif_omis, # minimum is the 2nd time dose administration
+                                           max=max_unif_omis), # maximum is the 4th time dose administration
+                                   digits=3)) %>%
     ungroup()
   
+  #compute the proportion 
+  id_proportion<-proportion * length(unique(data_1$ID))# proportion number of ID
+  nsel<-ifelse(id_proportion-floor(id_proportion)<0.5,# round the proportion to a lower bound or upper bound based on the decimal
+               floor(id_proportion),
+               ceiling(id_proportion))
   
-  id_proportion<-proportion * length(unique(data_1$ID))
-  nsel<-ifelse(id_proportion-floor(id_proportion)<0.5,floor(id_proportion),ceiling(id_proportion))
-  
-  
+  #sample the proportion <nsel>% of subjects
   mysel_reduction <- sample(data_1$ID, size = nsel, replace = F)
   
+  # compute the dose reduction for specific pacient
   data_1 %>%
     mutate(
-      IDflag_reduc = ifelse(ID %in% mysel_reduction, 1, 0),
-      DOSE_red = ifelse(IDflag_reduc == 1 & TIME >= TIME_unif_reduc, reduction, DOSE)
+      IDflag_reduc = ifelse(ID %in% mysel_reduction, 1, 0),# flag for the sampled ID
+      
+      # dose reduction starting from the Time larger than TIME_unif_reduc for the specific patients( IDflag_omis==1 )
+      DOSE_reduc = ifelse(IDflag_reduc == 1 & TIME >= TIME_unif_reduc, reduction, DOSE)
     ) 
   
 }
 
+full_data<-scenario_1(doses = 10 * c(1 / 6, 1 / 2, 1, 2, 3), 
+                      nweek = 9, 
+                      week = 7 * c(0, 6, 12, 18, 24, 30, 36, 42, 48, 52),
+                      n_N=5,
+                      lambda_N=4,
+                      lambda_0=4)
 
 data_bind_reduc <- rbind(
-  dose_reduction(initial_data = full_data, cohort = 3, proportion =  0.10, reduction = 5),
-  dose_reduction(initial_data = full_data, cohort = 4, proportion =  0.25, reduction = 10),
-  dose_reduction(initial_data = full_data, cohort = 5, proportion =  0.50, reduction = 20)
+  dose_reduction(initial_data = full_data, 
+                 cohort = 3, 
+                 proportion =  0.10, 
+                 reduction = 5,
+                 n_unif_omis=1,min_unif_omis=7*12/356, max_unif_omis= 7*24/356),
+  dose_reduction(initial_data = full_data,
+                 cohort = 4, 
+                 proportion =  0.25, 
+                 reduction = 10,
+                 n_unif_omis=1,min_unif_omis=7*12/356, max_unif_omis= 7*24/356),
+  dose_reduction(initial_data = full_data,
+                 cohort = 5, 
+                 proportion =  0.50, 
+                 reduction = 20,
+                 n_unif_omis=1,min_unif_omis=7*12/356, max_unif_omis= 7*24/356)
 )
 
 
+head(data_bind_reduc)
 
