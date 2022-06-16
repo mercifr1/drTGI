7d79cff94eb85ca852a18d450b229829708e0cd4
Maia-Iulia <muresan.maia@gmail.com>
2022-06-16 18:58:10 +0200

    rename the folder and update simulation

--- Maia/Rscripts/Scenario_2_dose_modification_simulation.R
+++ Maia/Rscripts/Scenario_2_dose_modification_simulation.R
@@ -1,19 +1,19 @@
-source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/functions.R")
 source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/SimODE.R")
-set.seed(567)
+source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/functions.R")
 
 
 
-do_once<-replicate(50,{
+
+
+unbalance_dose_modif<-replicate(10,{
   
 #step 1: generate the initial data set  
  
-  full_data<-scenario_1(doses = 10 * c(1 / 6, 1 / 2, 1, 2, 3), nweek = 9, week = 7 * c(0, 6, 12, 18, 24, 30, 36, 42, 48, 52),
-                      n_N=5,
-                      lambda_N=4,
-                      lambda_0=4)
+  full_data<-scenario_1(doses = 10*c(1 / 6, 1 / 2, 1, 2, 3), 
+                        nweek = 9,week = 7 * c(0, 6, 12, 18, 24, 30, 36, 42, 48, 52),
+                         n_N=5,lambda_N=4,lambda_0=4)
 
-#step 2: based on the initial dataset we make the dose reduction on the desired cohort
+#step 2: based on the initial data set apply the dose reduction on the desired cohort
 
   data_bind_reduc <- rbind(
   dose_reduction(initial_data = full_data, cohort = 3, proportion =  0.10, reduction = 5,
@@ -27,13 +27,13 @@ do_once<-replicate(50,{
 #setp 3: based on the initial dataset we make the dose omission for the desired cohort
 
   data_bind_omis <- rbind(
-  dose_omission(initial_data = full_data,cohort = 3,proportion = 0.10,omission = 0.001,
+  dose_omission(initial_data = full_data,cohort = 3,proportion = 0.10,omission = 0,
     n_unif_omis = 1, min_unif_omis = 7 * 12 / 356, max_unif_omis = 7 * 24 / 356
   ),
-  dose_omission(initial_data = full_data,cohort = 4,proportion = 0.25,omission = 0.001,
+  dose_omission(initial_data = full_data,cohort = 4,proportion = 0.25,omission = 0,
                 n_unif_omis = 1, min_unif_omis = 7 * 12 / 356, max_unif_omis = 7 * 24 / 356
   ),
-  dose_omission(initial_data = full_data,cohort = 5,proportion = 0.50,omission = 0.001,
+  dose_omission(initial_data = full_data,cohort = 5,proportion = 0.50,omission = 0,
     n_unif_omis = 1, min_unif_omis = 7 * 12 / 356, max_unif_omis = 7 * 24 / 356
   )
 )
@@ -43,7 +43,7 @@ do_once<-replicate(50,{
 
 
   omis_reduc_merge<-merge(data_bind_reduc,data_bind_omis,
-         by=c("ID","DOSE","coh","nt","id_obs","TIME","EVID","CMT"),sort = F)
+         by=c("ID","DOSE","COH","N_TIME","ID_OBS","TIME","EVID","CMT"),sort = F)
 
 
 
@@ -51,7 +51,7 @@ do_once<-replicate(50,{
 
   
   sim_data_test<-full_data%>%
-  filter(coh<=2)%>%
+  filter(COH<=2)%>%
   mutate(TIME_unif_reduc=0,
          IDflag_reduc=0,
          DOSE_reduc=DOSE,
@@ -66,7 +66,7 @@ do_once<-replicate(50,{
                        ifelse(IDflag_reduc==0 & DOSEflag_omis==1,DOSE_omis,
                               ifelse(IDflag_reduc==1 & DOSEflag_omis==1,DOSE_omis,DOSE))))%>%
   dplyr::rename(DOSE_INITIAL=DOSE,DOSE=DOSE_1)%>%
-  select(ID,DOSE,TIME,EVID,CMT,coh,nt,id_obs)
+  select(ID,DOSE,TIME,EVID,CMT,COH,N_TIME,ID_OBS)
   
 
   names(sim_data_test)<-toupper(names(sim_data_test))
@@ -76,42 +76,44 @@ list(new_data)
 })
 
 
-#create teh column with the replicate number 
-all_data<-list()
- for(i in 1:length(do_once)){
-   all_data[[i]]<-do_once[[i]]%>%mutate(REP=i)
+#Simulation
+
+
+unbalance_dose_modif_all<-list()
+ for(i in 1:length(unbalance_dose_modif)){
+   
+   unbalance_dose_modif_all[[i]]<-mrgsim_d(model_ode,as.data.frame(unbalance_dose_modif[[i]]),carry.out="DOSE,ALPHA,COH",end=-1,nocb=F)%>%
+   dplyr::mutate(DV=RESP+EPS_1,REP=i,ID2=paste0(REP, "-", ID))
+   
  }
+design<-unbalance_dose_modif_all
 
+library (plyr)
+scenario_2 <- plyr::ldply (design, data.frame)
 
-design_matrix<-all_data
-
-
-
-
-#'Simulation for the data set 
-set.seed(123)
-
-ode.coh<-mrgsim_d(model_ode,as.data.frame(sim_data_test),carry.out="DOSE,COH",end=-1,nocb=F)
-ode.coh<-ode.coh%>%mutate(DV=RESP+EPS_1)
-
-
-ode.coh2<-ode.coh%>%
+scenario_2_nadir<-scenario_2 %>%
   group_by(ID)%>%
   mutate(MIN=min(DV),# find the minimum response variable SLD
          Tmin=TIME[which.min(DV)],# find the time corresponding to the minimum value
-         FLAGt=ifelse(TIME<=Tmin,0,1),# flag==1 for TIME > Tmin 
+         FLAG_TIME=ifelse(TIME<=Tmin,0,1),# flag==1 for TIME > Tmin 
          MIN20=MIN+20*MIN/100,# compute the 20%incrise from nadir
-         FLAG20=ifelse(DV<=MIN20,0,1)# flag==1 for DV> MIN20
-  )%>%ungroup%>%as.data.frame()
-
+         FLAG_NADIR20=ifelse(DV<=MIN20,0,1),
+         FLAG_DVMIN=ifelse(FLAG_TIME==1 & FLAG_NADIR20==1,1,0)# flag==1 for DV> MIN20
+  )%>%
+  ungroup%>%
+  filter(FLAG_DVMIN==0)%>%
+  as.data.frame()
 
 
 #'plot
-p<-ggplot(ode.coh, aes(TIME,DV,group=ID,color=factor(COH)))+
+p_scenario2<-ggplot(filter(scenario_2_nadir,REP==2),aes(TIME,DV,group=ID,color=factor(DOSE)))+
   geom_line(size=1)+
   theme_bw()+
-  
   ylab("SLD")
-p
+p_scenario2
+
+
+
+
 
 
