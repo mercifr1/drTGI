bf2d332daadc9a3ea02c569c956e8b2f54e3f766
Maia-Iulia <muresan.maia@gmail.com>
2022-06-21 09:21:29 +0200

    Add description and libraries  for scenario 1

--- Maia/Rscripts/Scenario_1_function.R
+++ Maia/Rscripts/Scenario_1_function.R
@@ -1,3 +1,14 @@
+# Useful Libraries
+#install.packages("tidyr")
+library(tidyr)
+library(dplyr)
+#install.packages("purrr")
+library(purrr)
+#install.ackages("extraDistr")
+library(extraDistr) # for extra distributions : i.e truncated Poisson
+#install.packages("actuar")
+library(actuar) # fro extra distributions: i.e zero-truncated Poisson
+
 
 #' Scenario 1_function :  the unbalanced design matrix.
 #'
@@ -31,24 +42,24 @@ funtime <- function(ny, week) {
 
 scenario_1 <- function(doses, nweek, week,n_N,lambda_N, lambda_0) {
   #' doses: dose levels: a vector with the dose-cohorts
-  #' nweek: dose administration at every 6 week : vector with the time( parameter for the for the funtime())
-  #' week : #' total number of time  observations
-  #' n_N  : integer: parameter to generate truncated Poisson distr. for the number of individual/cohort (here n_N=5 dose-cohorts)
-  #' lambda_N: integer: parameter to generate the truncated Poisson distr.  
-  #' lambda_0: integer: parameter to generate  the zero-truncated Poisson distr for the number of observations per individual
+  #' nweek: dose administration at every 6 week : vector with  time points ( parameter for the  funtime())
+  #' week :  total number of time  observations: need to know length of vector nweek ( parameter for the  funtime())
+  #' n_N  : integer: parameter to generate truncated Poisson distr. rtpois() for the number of individual/cohort (here n_N=5 dose-cohorts)
+  #' lambda_N: integer: parameter to generate the truncated Poisson distr. rtpois() 
+  #' lambda_0: integer: parameter to generate  the zero-truncated Poisson distr rztpois() for the number of observations per individual
   #' NOTE: n_0 :=  total_number_indiv:  all the individual in the trail
   
-  
+  #' generate random numbers 
   individual_per_coh <- extraDistr::rtpois(n = n_N, lambda = lambda_N) # generate the number of individuals per cohort
   total_number_indiv <- sum(individual_per_coh) # total number of individual per trail
-  
+
   nb_obs_per_indiv <- actuar::rztpois(n = total_number_indiv, lambda = lambda_0) # generate number of observations per individual
 
   #' construct the data with the number of observations per individual
   id_obs_data <- tibble(ID = 1:total_number_indiv) %>%
     split(.$ID) %>%
     purrr::map_dfr(., ~ funtime(ny = nweek, week = week), .id = "ID") %>%
-    mutate(ID_OBS = rep(nb_obs_per_indiv, each = max(.$N_TIME) + 1)) %>%
+    dplyr::mutate(ID_OBS = rep(nb_obs_per_indiv, each = max(.$N_TIME) + 1)) %>%
     dplyr::filter(N_TIME <= ID_OBS)
 
   #' construct the data with the  number of individual per cohort design matrix
@@ -56,7 +67,7 @@ scenario_1 <- function(doses, nweek, week,n_N,lambda_N, lambda_0) {
   #' 
   id_cohort_data <- data.frame(DOSE = round(doses, 2), COH = 1:length(doses), ID.PER.COH = individual_per_coh)%>%
     tidyr::uncount(., ID.PER.COH) %>%
-    mutate(ID = 1:total_number_indiv)
+    dplyr::mutate(ID = 1:total_number_indiv)
   #' NOTE: uncount() : duplicating rows according to a weighting variable (or expression):performs the opposite to dplyr::count()
 
   #' merge the 2 data set by ID and keep all the columns from the first data set 
