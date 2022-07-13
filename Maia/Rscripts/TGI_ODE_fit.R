118398404699725be8cdf01cfcac20e0a046480c
Maia-Iulia <muresan.maia@gmail.com>
2022-07-13 17:00:09 +0200

    R file with the data for stan and fit the model

--- Maia/Rscripts/TGI_ODE_fit.R
+++ Maia/Rscripts/TGI_ODE_fit.R
@@ -3,87 +3,79 @@ source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/Scenario_2_3_4_dose_modifi
 library(rstan)
 library(cmdstanr)
 
-dim(design_flat)
 #' prepare the data for stan 
+
+# import the simulated dataset
 data_for_fit<-design_flat%>%
   filter(REP==1)%>%
-  dplyr::mutate(TIME=ifelse(TIME<0.1,0,TIME))
-         T
-data_for_fit$timeInd<-seq(1:nrow(data_for_fit))
+  dplyr::mutate(TIME=ifelse(TIME<0.1,0,TIME),N_TIME=N_TIME)%>%
+  select(ID,TIME,N_TIME,DOSE,DV,BASE,KG,KS0,GAMMA,ALPHA,EPS_1,ID_OBS)
 
-data <- list( N = nrow(data_for_fit),
-              J = length(unique(data_for_fit$ID)),
-              y = data_for_fit$DV,
-              t0 = 0.0,
-              timeInd= data_for_fit%>%select(timeInd)%>%pull(),
-              ts=data_for_fit%>%select(TIME)%>%filter(TIME>0)%>%pull(),
-              dose=data_for_fit$DOSE,
-              T=data_for_fit %>% group_by(ID) %>% summarise("maxx" = max(N_TIME)) %>% select(maxx) %>% pull(),
-              nTime = data_for_fit %>% group_by(ID) %>% summarise("maxx" = max(N_TIME)) %>% select(maxx) %>% pull()
+#' buil a function to filter the *initial time point 0 (TRUE/FALSE)
+data_prep <- function(data, no_0 =T){
+  if(no_0){
+    data_res <- filter(data, TIME !=0)
+  } else {
+    data_res <- data
+  }
+  data_res
+}
+
+
+gk_data <- data_prep(data_for_fit, no_0 = T)
+nrow(gk_data)
+
+data_gk <- list(N = nrow(gk_data), # nb. observations
+                uniqueT=nrow(gk_data),
+                J = length(unique(gk_data$ID)), # nr. individuals
+                
+                y = gk_data$DV, # response vector
+                t0 = 0.00, # initial time point
+                ts=gk_data%>%select(TIME)%>%pull(), # time points
+                #dose=data_for_fit$DOSE, # dose vector
+                nTime=gk_data %>% group_by(ID) %>% summarise("maxx" = max(N_TIME)) %>% select(maxx) %>% pull(),# nb. of observations per individual
+                timeInd=gk_data %>% group_by(ID)%>% select(N_TIME) %>% pull()
 )
+
+
+# initials values 
+inits_1 <- list(tvkg=0.4,
+                tvks0=0.4, 
+              x_r=0.0,
+              x_i=0,
+              tvgamma= .2,
+              tvalpha= .2,
+              tvbase=70,
+              
+              omega_kg =.5,
+              omega_ks0 =.5,
+              omega_gamma=.5,
+              omega_alpha=.5,
+              omega_base=.5,
+              sigma=0.5,
+              eta_kg = rep(.5, 23),
+              eta_ks0= rep(.5, 23),
+              eta_gamma=rep(.5, 23),
+              eta_alpha= rep(.5, 23),
+              eta_base= rep(.5, 23)
+              
+              
+              )
+
+# repeate the initials values to be the same as the number of chains in the sample Stan code
+inits <- list(inits_1, inits_1, inits_1, inits_1)
 
 
 #' cmdstan compile
-mod<-cmdstan_model("Maia/Rscripts/TGI_ODE_stan_test_3.stan")
+#' 
+mod<-cmdstan_model("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/TGI_stan_random_effects.stan")
+
+# check if teh syntax is correct
+mod$check_syntax(pedantic = TRUE)
 
 
+# fit
+fit <- mod$sample(data = data_gk , iter_sampling = 2000,iter_warmup = 1000,chains = 4,parallel_chains = 4, refresh = 10, init = inits)
 
-mod$print()
-mod$exe_file()
-
-fit <- mod$sample(
-  data = data, 
-  seed = 42L, 
-  chains = 4, 
-  parallel_chains = 4,
-  refresh = 0 # print update every 500 iters
-)
-
-
-
-#' Remarks:
-#'Delay compilation:
-#' in case we w ant to delay compilation when creating the CmdStanModel object
-mod_delay<-cmdstan_model("Maia/Rscripts/TGI_ODE_stan_test_3.stan",compile = FALSE)
-mod_delay$exe_file()
-mod_delay$compile()
-mod_delay$exe_file()
-
-#'Pedantic check:
-#' in case we want to check if there are any invalid syntax and also it enabled
-#'    the check will also warn you about other potential issues in your as:
-#' 1. distribution usages issues
-#' 2. Unused parameter
-#' 3. Large or small constant in a distr
-#' 4. Control flow depends on a parameter: branching control (if/else)
-#' 5. Variable is used before assignment
-#' 6. strict or nonsensical parameter bounds
-
-mod_pedantic<-cmdstan_model("Maia/Rscripts/TGI_ODE_stan_test_3.stan",pedantic=TRUE)
-
-#' in case we want to turn on pedantic model separately from compilation:
-mod_pedantic$check_syntax(pedantic = TRUE) 
-#'or 
-
-mod_pedantic <- cmdstan_model("Maia/Rscripts/TGI_ODE_stan_test_3.stan", compile = FALSE)
-mod_pedantic$check_syntax(pedantic = TRUE)
-
-#' Stan model variables
-variables<-mod$variables()
-
-names(variables)
-names(variables$data)
-names(variables$parameters)
-names(variables$transformed_parameters)
-names(variables$generated_quantities)
-
-
-variables$data$J
-variables$data$sigma
-variables$parameters$kg
-variables$transformed_parameters$theta
-
-
-
-
+res <- fit$summary()
 
