8d9d1da30d9f34dd52e6b2361f7e28a21cd82350
Maia-Iulia <muresan.maia@gmail.com>
2022-07-27 21:35:12 +0200

    simulation of SLD for balanced design matrix scenario 0

--- Maia/Rscripts/Simulation_SLD_Scenario_0.R
+++ Maia/Rscripts/Simulation_SLD_Scenario_0.R
@@ -1,6 +1,26 @@
+###############################################################################
+
+# TGI : Simulation SLD profiles from  ODE with mrgsolve 
+
+# Scenario 0 balanced design matrix  
+# 5 dose-cohorts with same number of observations for each individual
+
+# 31-07-2022
+
+# Maia Muresan
+
+################################################################################
+# source ODE model 
 source("C:/Users/muresai1/Desktop/drTGI/Maia/Rscripts/SimODE.R")
 
-replicate_balance_data<-replicate(2,{
+#libraries
+library (plyr)
+
+
+# Make replications of the design matrix 
+
+
+replicate_balance_data<-replicate(100,{
 #dose-cohorts
 doses = 10 * c(1 / 6, 1 / 2, 1, 2, 3)
 # number of observations
@@ -9,7 +29,7 @@ nTA<-9
 skel_6w<-7*c(0,6,12,18,24,30,36,42,48,52) 
 
 
-# generate the number of individuals per cohor
+# generate the number of individuals per cohort
 individual_per_coh <- extraDistr::rtpois(n = 5, lambda = 4) 
 
 # total number of individual per trail
@@ -35,13 +55,16 @@ id_cohort_data <- data.frame(DOSE = round(doses, 2),
   mutate(ID = 1:total_number_indiv)
 
 #' merge the 2 data set by ID and keep all the columns from the first data set 
-balance_data <- merge(x=id_cohort_data, y=id_observation_data, by = "ID", all.x = T)
+balance_data <- merge(x=id_cohort_data, y=id_observation_data, by = "ID", all.x = T)%>%
+  dplyr::mutate(EVID=2,CMT=1)
+
 list(balance_data)
 })
 
 
-#create teh column with the replicate number 
+# simulate the SLD( DV ) profile  values
 balance_all_data<-list()
+
 for(i in 1:length(replicate_balance_data)){
   
   balance_all_data[[i]]<-mrgsim_d(model_ode,as.data.frame(replicate_balance_data[[i]]),carry.out="DOSE,ALPHA,COH",end=-1,nocb=F)%>%
@@ -51,7 +74,7 @@ for(i in 1:length(replicate_balance_data)){
 
 balance_design_matrix<-balance_all_data
 
-library (plyr)
+
 final_balance_design_matrix <- plyr::ldply (balance_design_matrix, data.frame)
 
 
@@ -65,14 +88,15 @@ final_balance_nadir<-final_balance_design_matrix %>%
          FLAG_NADIR20=ifelse(DV<=MIN20,0,1),
          FLAG_DVMIN=ifelse(FLAG_TIME==1 & FLAG_NADIR20==1,1,0)# flag==1 for DV> MIN20
   )%>%
-  ungroup%>%filter(FLAG_DVMIN==0)
+  ungroup%>%
+  filter(FLAG_DVMIN==0)%>%
   as.data.frame()
 
   
   #'plot
-p_balance<-ggplot(filter(final_balance_nadir,REP==1),aes(TIME,DV,group=ID,color=factor(DOSE)))+
+p_balance<-ggplot(filter(final_balance_nadir,REP==2),aes(TIME,DV,group=ID,color=factor(DOSE)))+
     geom_line(size=1)+
     theme_bw()+
-    ylab("SLD")
+    ylab("SLD")+labs(title="Scenario 0: Balance deisgn matrix with flat dose")
 p_balance
   
